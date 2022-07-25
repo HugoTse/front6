08625dc9a63a468fdf24ed516f88f0b259c9ff61
Hugo Tse <hugotse8@gmail.com>
2022-07-25 15:10:32 -0700

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "front6d69d5e29": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+  Grid
+} from "@aws-amplify/ui-react";
+
+import { Amplify, API, Storage } from 'aws-amplify';
+import React, { useState, useEffect } from "react";
+
+function App({ signOut }) {
+
+  // For uploadImage
+  async function onChange(e) {
+    e.preventDefault();
+    // If there are no files, return
+    if (!e.target.files[0]) return
+    // Set the variable file to the uploaded file
+    const file = e.target.files[0];
+    // Set the key for the file
+    const imageKey = 'image/raw/' + file.name;
+    // setIr(imageKey);
+    // Log the key
+    console.log(imageKey);
+    // try {
+    //   // Place the image in the bucket
+    //   await Storage.put(imageKey, file, {
+    //     contentType: "image/png", // contentType is optional
+    //   })
+    //   // This returns the key
+    //   .then((res) => 
+    //     console.log(res)
+    //   );
+    // } catch (error) {
+    //   console.log("Error uploading file: ", error);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card
+        columnStart="1"
+        columnEnd="-1"
+      >
+       <Heading level={1}>Nautilus Marketing</Heading> 
+      </Card>
+
+      <div className='mainContent'>
+        {/* Upload Image */}
+        <div className='formDiv'>
+          <input
+            type="file"
+            onChange={onChange}
+          />
+        </div>
+
+        {/* Inputs */}
+        <Heading level={3}>Comment Metadata</Heading> 
+
+
+        <div className='formDiv'>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
+      </div>
+   
+
+    </>
+
+
   );
 }
 
-export default App;
+export default withAuthenticator(App);
